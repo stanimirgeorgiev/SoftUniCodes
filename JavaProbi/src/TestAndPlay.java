83f8c8aa9816b976fda5fb4bdd86eaf90ad98daf
Stanimir Georgiev <az.stanimir@gmail.com>
2015-01-20 21:09:04 +0200

    Update

--- JavaProbi/src/TestAndPlay.java
+++ JavaProbi/src/TestAndPlay.java
@@ -2,6 +2,19 @@ class TestAndPlay {
 	public static void main(String[] args) {
 	System.out.print("\',\\,\",\\");
 	System.out.println("\',\\,\",\\");
-
+	String firstString = "Proba";
+	String secondString = "Second";
+	int num = 22;
+	char charTest = 'c';
+	float floatTest = 12.56983f;
+	System.out.println("Proba" + (2 + 2));
+	System.out.print("Proba" + 2 + 2 + "\n");
+	System.out.printf("%s\n", firstString);
+	System.out.printf("%s is equals to %d.\n", firstString, num);
+	System.out.printf("%b is equals to %o.\n", firstString, num);
+	System.out.printf("%c is equals to %x.\n", charTest, num);
+	System.out.printf("%S is equals to %.4f .\n", firstString, floatTest);
+	System.out.printf("%n is equals to %d.\n", num);
+	
 }
 }
